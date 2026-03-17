c5d7dad2e472b83ab18b81e0b010035a00c4f217
Victor <GGVictor7373@outlook.com>
2026-03-17 09:31:16 +0100

    Fix Node.js dependency installation by replacing npm ci with npm install for improved flexibility

--- lib/deployment-files.sh
+++ lib/deployment-files.sh
@@ -78,8 +78,8 @@ RUN COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_MEMORY_LIMIT=-1 \
     && composer clear-cache
 
 # Install Node.js dependencies and compile assets (JS/CSS)
-RUN npm cache clean --force \
-    && npm ci --legacy-peer-deps \
+RUN rm -rf node_modules package-lock.json \
+    && npm install --no-optional --legacy-peer-deps \
     && npm run production \
     && rm -rf node_modules
 
